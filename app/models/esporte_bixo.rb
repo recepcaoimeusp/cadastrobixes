26e59f94349d7370dcae77b2445530b34109f94a
Victor Portella <visportella@gmail.com>
2014-02-07 17:13:42 -0200

    Mudanças em algumas views

--- app/models/esporte_bixo.rb
+++ app/models/esporte_bixo.rb
@@ -1,3 +1,9 @@
 class EsporteBixo < ActiveRecord::Base
   attr_accessible :bixo_id, :esporte_id
+
+  belongs_to :bixo
+  belongs_to :esporte
+
+  validates :bixo_id, :presence => :true
+  validates :esporte_id, :presence => :true
 end
