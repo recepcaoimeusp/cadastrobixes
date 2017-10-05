1e9cb41e66bac66fa9d2a1207db92a0b5971a322
Erick Cordeiro Ozaki <erick.ozaki@gmail.com>
2017-10-05 14:44:53 -0300

    Other validation tests for Bixo

--- spec/models/bixo_spec.rb
+++ spec/models/bixo_spec.rb
@@ -2,4 +2,7 @@ require 'rails_helper'
 
 describe Bixo do
   it { should validate_presence_of(:nome) }
+  it { should validate_presence_of(:telefone) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:curso) }
 end
