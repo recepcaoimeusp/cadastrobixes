528a65c753641e723cf4537cc426fd39e5ae324f
Erick Cordeiro Ozaki <erick.ozaki@gmail.com>
2017-10-05 14:34:28 -0300

    Validating Bixo model

--- app/models/bixo.rb
+++ app/models/bixo.rb
@@ -1,2 +1,8 @@
 class Bixo < ApplicationRecord
+  validates :nome, presence: true
+  validates :telefone, presence: true
+  validates :email, presence: true
+  validates :curso, presence: true
+
+  enum curso: [:lic, :pura, :bcc, :estat, :aplicada, :bmac].freeze
 end
