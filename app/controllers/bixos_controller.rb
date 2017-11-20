b53ad214ea8f0d72d2c3deaf6e462357a2d235f0
Erick Cordeiro Ozaki <erick.ozaki@gmail.com>
2017-11-20 17:53:54 -0200

    Better styling a few pages

--- app/controllers/bixos_controller.rb
+++ app/controllers/bixos_controller.rb
@@ -69,6 +69,7 @@ class BixosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def bixo_params
+      params[:bixo][:curso] = params[:bixo][:curso].to_i
       params.require(:bixo).permit(:nome, :email, :telefone, :curso)
     end
 end
