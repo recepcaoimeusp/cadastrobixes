139557eedde77011b00dfe1afa4a5292d8ee36f2
Erick Cordeiro Ozaki <erick.cordeiro@locaweb.com.br>
2018-02-10 12:29:36 -0200

    Tests adequation to new behavior

--- spec/models/venda_spec.rb
+++ spec/models/venda_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Venda, type: :model do
-  it { should validate_presence_of(:cor) }
-  it { should validate_presence_of(:tamanho) }
+  context `on update` do
+    let(:venda) { Venda.create(bixo_id: 1) }
+
+    it 'should contain errors and not save' do
+      expect(venda.update(tamanho: nil, cor: 1)).to be_falsey
+      expect(venda.errors).not_to be_nil
+    end
+    
+    it 'should contain errors and not save' do
+      expect(venda.update(cor: nil, tamanho: 1)).to be_falsey
+      expect(venda.errors).not_to be_nil
+    end
+  end
 end
