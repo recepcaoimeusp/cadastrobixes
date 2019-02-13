8329d02c496f2bf9e6b0e71d3319b1cb6fccd2d9
Erick Cordeiro Ozaki <erick.cordeiro@locaweb.com.br>
2019-02-13 17:30:52 -0200

    Removing authentication and sales routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  http_basic_authenticate_with name: "comissao", password: "bixoamigonaocomida"
 
   # GET /admin
   def admin
--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,6 @@ Rails.application.routes.draw do
   post '/bixos/:id/modalidades' => 'bixos#modify_modalidades', as: :modify_bixo_modalidades
   post '/bixos/:id/nova_venda' => 'bixos#nova_venda', as: :nova_venda
 
-  put '/vendas/:id/novo_pagamento' => 'vendas#novo_pagamento', as: :novo_pagamento
-  get '/vendas/inadimplentes' => 'vendas#inadimplentes', as: :inadimplentes
-  get '/vendas/sem_caneca' => 'vendas#sem_caneca', as: :sem_caneca
-
   get '/modalidades/contatos' => 'modalidades#contatos', as: :contato_modalidades
 
   get '/admin' => 'application#admin', as: :admin
